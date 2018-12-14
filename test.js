87a85ea44b40194aec77c16532cc81a58c68262c
Lian Jian Yang <jianyang.lian@gmail.com>
2018-12-14 19:25:03 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,62 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org/",
+  "@type": "Product",
+  "name": "DRESS BLUES LEVI'S® HOUSEMARK TEE",
+  "url": "https://levi.com.my/product/17783-0139/",
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "4.4",
+    "reviewCount": "89"
+  },
+  "brand": {
+    "@type": "Organization",
+    "name": "Levi Strauss & Co."
+  },
+  "description": "This essential men’s T-shirt features our iconic batwing logo graphic for the right amount of detail. Made from soft essential jersey, this short-sleeve crew neck tee in Dress Blues is comfortable and versatile, so you can wear it anytime, anywhere.",
+  "material": "100% Cotton",
+  "mpn": "17783-0139",
+  "image": [
+    "https://levi.com.my/wp-content/uploads/2017/12/177830139_1-860x860.jpg",
+    "https://levi.com.my/wp-content/uploads/2017/12/177830139_2-860x860.jpg",
+    "https://levi.com.my/wp-content/uploads/2017/12/177830139_3-860x860.jpg",
+    "https://levi.com.my/wp-content/uploads/2017/12/177830139_4-860x860.jpg",
+    "https://levi.com.my/wp-content/uploads/2017/12/177830139_5-860x860.jpg",
+    "https://levi.com.my/wp-content/uploads/2017/12/177830139_6-860x860.jpg",
+    "https://levi.com.my/wp-content/uploads/2017/12/177830139_7-860x860.jpg",
+    "https://levi.com.my/wp-content/uploads/2017/12/177830139_8-860x860.jpg"
+  ],
+  "itemCondition": "http://schema.org/NewCondition",
+  "offers": {
+    "@type": "Offer",
+    "availability": "http://schema.org/InStock", 
+    "price": "65.00",
+    "priceCurrency": "MYR",
+    "url": "https://levi.com.my/product/17783-0139/",
+    "priceValidUntil": "2020-11-05",
+    "itemCondition": "http://schema.org/NewCondition"
+  },
+  "audience": {
+    "@type": "PeopleAudience",
+    "suggestedGender": "male"
+  },
+  "color": "DRESS BLUES",
+  "sku": "17783-0139",
+  "review": [
+    {
+      "@type": "Review",
+      "author": "Name",
+      "datePublished": "2006-05-04",
+      "name": "",
+      "reviewBody": "",
+      "reviewRating": {
+            "@type": "Rating",
+            "bestRating": "0",
+            "ratingValue": "0",
+            "worstRating": "0"
+           }
+    }
+  ]
+  },
+}
+</script>
